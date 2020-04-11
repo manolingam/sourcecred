190d84ee13568e94a1f0f44b8749d255f772d28c
Hammad Jutt <jutt@ualberta.ca>
2020-04-10 18:36:41 -0600

    Add grain harvest module
    This adds the `grain/harvest.js` module, which contains logic for computing "harvests",
    aka grain distributions, based on cred scores.
    
    The resultant Harvest type contains the receipts (who gets how much Grain) and the
    strategy informing the payout. We can then include these directly into the ledger
    to keep a record of the grain balances.
    
    The code is pretty well tested, hitting a number of potential edge cases in the
    harvest logic.
    
    Test plan: Inspect attached unit tests, verify CI passes.
    
    Paired with @decentralion

---
+++ src/grain/harvest.js
@@ -0,0 +1,220 @@
+// @flow
+
+/**
+ * In SourceCred, projects regularly distribute Grain to contributors based on
+ * their Cred scores. This is called a "Harvest".
+ *
+ * This module contains the logic for calculating harvest amounts for
+ * contributors. A harvest contains "receipts" showing how much Grain each
+ * contributor will receive, the timestamp of the harvest in question, and the
+ * "strategy" used to distribute grain.
+ *
+ * Currently we support two strategies:
+ * - FAST, which distributes a fixed amount of grain based on the cred scores
+ * in the most recent completed time interval
+ * - FAIR, which distributes a fixed amount of grain based on cred scores
+ * across
+ * all time, prioritizing paying people who were under-paid historically (i.e.
+ * their lifetime earnings are lower than we would expect given their current
+ * cred score)
+ *
+ * In all cases, the timestamp for the harvest is used to determine which cred
+ * scores are in scope. For example, if you create a fast harvest with a
+ * timestamp in the past, it will reward people with cred in the past time
+ * period, not the current one.
+ */
+import {sum} from "d3-array";
+import {mapToArray} from "../util/map";
+import {type NodeAddressT} from "../core/graph";
+import {type Grain, multiplyFloat, ZERO} from "./grain";
+
+export const HARVEST_VERSION_1 = 1;
+
+export type HarvestStrategy = FastV1 | FairV1;
+
+export type FastV1 = {|
+  +type: "FAST",
+  +version: number,
+  +amount: Grain,
+|};
+
+export type FairV1 = {|
+  +type: "FAIR",
+  +version: number,
+  +amount: Grain,
+|};
+
+export type GrainReceipt = {|+address: NodeAddressT, amount: Grain|};
+
+export type HarvestV1 = {|
+  +type: "HARVEST",
+  +timestampMs: number,
+  +version: number,
+  +strategy: HarvestStrategy,
+  +receipts: $ReadOnlyArray<GrainReceipt>,
+|};
+
+export type CredTimeSlice = {|
+  +intervalEndMs: number,
+  +cred: Map<NodeAddressT, number>,
+|};
+
+export type CredHistory = $ReadOnlyArray<CredTimeSlice>;
+
+/**
+ * Compute a full Harvest given:
+ * - the strategy we're using
+ * - the full cred history for all users
+ * - the lifetime earnings of all users
+ * - the timestamp for the harvest
+ */
+export function harvest(
+  strategy: HarvestStrategy,
+  credHistory: CredHistory,
+  earnings: Map<NodeAddressT, Grain>,
+  timestampMs: number
+): HarvestV1 {
+  const filteredSlices = credHistory.filter(
+    (s) => s.intervalEndMs <= timestampMs
+  );
+  if (!filteredSlices.length) {
+    return {
+      type: "HARVEST",
+      timestampMs,
+      version: HARVEST_VERSION_1,
+      strategy,
+      receipts: [],
+    };
+  }
+
+  const receipts: $ReadOnlyArray<GrainReceipt> = (function () {
+    switch (strategy.type) {
+      case "FAST":
+        if (strategy.version !== 1) {
+          throw new Error(`Unsupported FAST strategy: ${strategy.version}`);
+        }
+        const lastSlice = filteredSlices[filteredSlices.length - 1];
+        return computeFastReceipts(strategy.amount, lastSlice.cred);
+      case "FAIR":
+        if (strategy.version !== 1) {
+          throw new Error(`Unsupported FAIR strategy: ${strategy.version}`);
+        }
+        const totalCred = new Map();
+        for (const {cred} of filteredSlices) {
+          for (const [address, ownCred] of cred.entries()) {
+            const existingCred = totalCred.get(address) || 0;
+            totalCred.set(address, existingCred + ownCred);
+          }
+        }
+        return computeFairReceipts(strategy.amount, totalCred, earnings);
+      default:
+        throw new Error(`Unexpected type ${(strategy.type: empty)}`);
+    }
+  })();
+
+  return {
+    type: "HARVEST",
+    version: HARVEST_VERSION_1,
+    strategy,
+    receipts,
+    timestampMs,
+  };
+}
+
+/**
+ * Split a grain amount in proportion to the provided scores
+ */
+function computeFastReceipts(
+  harvestAmount: Grain,
+  cred: Map<NodeAddressT, number>
+): $ReadOnlyArray<GrainReceipt> {
+  if (harvestAmount < ZERO) {
+    throw new Error(`invalid harvestAmount: ${String(harvestAmount)}`);
+  }
+
+  const totalCred = sum(Array.from(cred.values()));
+  if (totalCred === 0) {
+    return [];
+  }
+
+  return mapToArray(cred, ([address, cred]) => ({
+    address,
+    amount: multiplyFloat(harvestAmount, cred / totalCred),
+  }));
+}
+
+/**
+ * Distribute a fixed amount of Grain to the users who were "most underpaid".
+ *
+ * We consider a user underpaid if they have recieved a smaller proportion of
+ * past earnings than their share of score. They are fairly paid if their
+ * proportion of earnings is equal to their score share, and they are overpaid
+ * if their proportion of earnings is higher than their share of the score.
+ *
+ * We start by imagining a hypothetical world, where the entire grain supply of
+ * the project (including this harvest) were distributed according to the
+ * current scores. Based on this, we can calculate the "fair" lifetime earnings
+ * for each participant. Usually, some will be "underpaid" (they recieved less
+ * than this amount) and others are "overpaid".
+ *
+ * We can sum across all users who were underpaid to find the "total
+ * underpayment". As an invariant, `totalUnderpayment = harvestAmount +
+ * totalOverpayment`, since no-one has yet been paid the harvestAmount, and
+ * beyond that every grain of overpayment to one actor is underpayment for a
+ * different actor.
+ *
+ * Now that we've calculated each actor's underpayment, and the total
+ * underpayment, we divide the harvest's grain amount across users in
+ * proportion to their underpayment.
+ *
+ * You should use this harvest when you want to divide a fixed amount of grain
+ * across participants in a way that aligns long-term payment with total cred
+ * scores.
+ */
+function computeFairReceipts(
+  harvestAmount: Grain,
+  credMap: Map<NodeAddressT, number>,
+  earnings: Map<NodeAddressT, Grain>
+): $ReadOnlyArray<GrainReceipt> {
+  let totalEarnings = ZERO;
+  for (const e of earnings.values()) {
+    totalEarnings += e;
+  }
+  let totalCred = 0;
+  for (const s of credMap.values()) {
+    totalCred += s;
+  }
+  if (totalCred === 0) {
+    return [];
+  }
+
+  const targetGrainPerCred = multiplyFloat(
+    totalEarnings + harvestAmount,
+    1 / totalCred
+  );
+
+  let totalUnderpayment = ZERO;
+  const userUnderpayment: Map<NodeAddressT, Grain> = new Map();
+  const addresses = new Set([...credMap.keys(), ...earnings.keys()]);
+
+  for (const addr of addresses) {
+    const earned = earnings.get(addr) || ZERO;
+    const cred = credMap.get(addr) || 0;
+
+    const target = multiplyFloat(targetGrainPerCred, cred);
+    if (target > earned) {
+      const underpayment = target - earned;
+      userUnderpayment.set(addr, underpayment);
+      totalUnderpayment += underpayment;
+    }
+  }
+
+  return mapToArray(userUnderpayment, ([address, underpayment]) => {
+    const underpaymentProportion =
+      Number(underpayment) / Number(totalUnderpayment);
+    return {
+      address,
+      amount: multiplyFloat(harvestAmount, underpaymentProportion),
+    };
+  });
+}
---
+++ src/grain/harvest.test.js
@@ -0,0 +1,331 @@
+// @flow
+
+import {NodeAddress} from "../core/graph";
+import {ONE, ZERO, fromApproximateFloat, format} from "./grain";
+import {HARVEST_VERSION_1, harvest} from "./harvest";
+import deepFreeze from "deep-freeze";
+import type {
+  CredHistory,
+  FairV1,
+  FastV1,
+  GrainReceipt,
+  HarvestStrategy,
+  HarvestV1,
+} from "./harvest";
+import type {Grain} from "./grain";
+
+describe("src/grain/harvest", () => {
+  const foo = NodeAddress.fromParts(["foo"]);
+  const bar = NodeAddress.fromParts(["bar"]);
+
+  const timestampMs = 1000;
+
+  const credHistory: CredHistory = deepFreeze([
+    {
+      intervalEndMs: 10,
+      cred: new Map([
+        [foo, 9],
+        [bar, 1],
+      ]),
+    },
+    {
+      intervalEndMs: 20,
+      cred: new Map([
+        [foo, 1],
+        [bar, 1],
+      ]),
+    },
+    {intervalEndMs: 30, cred: new Map([[bar, 2]])},
+  ]);
+  function fmt(g: Grain) {
+    return format(g, 3);
+  }
+  function safeReceipts(receipts: $ReadOnlyArray<GrainReceipt>) {
+    return receipts.map(({address, amount}) => ({
+      address,
+      amount: fmt(amount),
+    }));
+  }
+  function safeStrategy(strat: HarvestStrategy) {
+    return {...strat, amount: fmt(strat.amount)};
+  }
+  function safeHarvest(harvest: HarvestV1) {
+    return {
+      ...harvest,
+      strategy: safeStrategy(harvest.strategy),
+      receipts: safeReceipts(harvest.receipts),
+    };
+  }
+  function expectHarvestsEqual(h1, h2) {
+    expect(safeHarvest(h1)).toEqual(safeHarvest(h2));
+  }
+
+  describe("fastHarvest", () => {
+    const strategy: FastV1 = deepFreeze({
+      type: "FAST",
+      amount: ONE,
+      version: 1,
+    });
+
+    describe("it should return an empty harvest when", () => {
+      const emptyHarvest = deepFreeze({
+        type: "HARVEST",
+        version: HARVEST_VERSION_1,
+        receipts: [],
+        strategy,
+        timestampMs,
+      });
+
+      it("there are no cred scores at all", () => {
+        const actual = harvest(strategy, [], new Map(), timestampMs);
+
+        expectHarvestsEqual(actual, emptyHarvest);
+      });
+
+      it("all cred scores are from the future", () => {
+        const actual = harvest(strategy, credHistory, new Map(), 0);
+        expectHarvestsEqual(actual, {...emptyHarvest, timestampMs: 0});
+      });
+
+      it("all the cred sums to 0", () => {
+        const actual = harvest(
+          strategy,
+          [
+            {
+              intervalEndMs: timestampMs - 500,
+              cred: new Map([
+                [foo, 0],
+                [bar, 0],
+              ]),
+            },
+          ],
+          new Map(),
+          timestampMs
+        );
+
+        expectHarvestsEqual(actual, emptyHarvest);
+      });
+    });
+
+    const createFastHarvest = (
+      timestampMs: number,
+      receipts: $ReadOnlyArray<GrainReceipt>
+    ) => {
+      return {
+        type: "HARVEST",
+        version: HARVEST_VERSION_1,
+        receipts,
+        strategy,
+        timestampMs,
+      };
+    };
+
+    it("handles an interval in the middle", () => {
+      const result = harvest(strategy, credHistory, new Map(), 20);
+      // $ExpectFlowError
+      const HALF = ONE / 2n;
+      const expectedReceipts = [
+        {address: foo, amount: HALF},
+        {address: bar, amount: HALF},
+      ];
+      const expectedHarvest = createFastHarvest(20, expectedReceipts);
+      expectHarvestsEqual(result, expectedHarvest);
+    });
+    it("handles an interval with un-even cred distribution", () => {
+      const result = harvest(strategy, credHistory, new Map(), 12);
+      // $ExpectFlowError
+      const ONE_TENTH = ONE / 10n;
+      const NINE_TENTHS = ONE - ONE_TENTH;
+      const expectedReceipts = [
+        {address: foo, amount: NINE_TENTHS},
+        {address: bar, amount: ONE_TENTH},
+      ];
+      const expectedHarvest = createFastHarvest(12, expectedReceipts);
+      expectHarvestsEqual(result, expectedHarvest);
+    });
+    it("handles an interval at the end", () => {
+      const result = harvest(strategy, credHistory, new Map(), 1000);
+      const expectedReceipts = [{address: bar, amount: ONE}];
+      const expectedHarvest = createFastHarvest(1000, expectedReceipts);
+      expectHarvestsEqual(result, expectedHarvest);
+    });
+  });
+
+  describe("fairHarvest", () => {
+    const strategy = deepFreeze({
+      type: "FAIR",
+      amount: fromApproximateFloat(14),
+      version: 1,
+    });
+
+    const createFairHarvest = (
+      timestampMs: number,
+      receipts: $ReadOnlyArray<GrainReceipt>,
+      strategy: FairV1
+    ) => {
+      return {
+        type: "HARVEST",
+        version: HARVEST_VERSION_1,
+        receipts,
+        strategy,
+        timestampMs,
+      };
+    };
+
+    describe("it should return an empty harvest when", () => {
+      const emptyHarvest = deepFreeze({
+        type: "HARVEST",
+        version: HARVEST_VERSION_1,
+        receipts: [],
+        strategy,
+        timestampMs,
+      });
+
+      it("there are no cred scores at all", () => {
+        const actual = harvest(strategy, [], new Map(), timestampMs);
+
+        expectHarvestsEqual(actual, emptyHarvest);
+      });
+
+      it("all cred scores are from the future", () => {
+        const actual = harvest(strategy, credHistory, new Map(), 0);
+        expectHarvestsEqual(actual, {...emptyHarvest, timestampMs: 0});
+      });
+
+      it("all the cred sums to 0", () => {
+        const actual = harvest(
+          strategy,
+          [
+            {
+              intervalEndMs: timestampMs - 500,
+              cred: new Map([
+                [foo, 0],
+                [bar, 0],
+              ]),
+            },
+          ],
+          new Map(),
+          timestampMs
+        );
+
+        expectHarvestsEqual(actual, emptyHarvest);
+      });
+    });
+
+    it("should only pay Foo if Foo is sufficiently underpaid", () => {
+      const earnings = new Map([
+        [foo, ZERO],
+        [bar, fromApproximateFloat(99)],
+      ]);
+      const expectedReceipts = [
+        {address: foo, amount: fromApproximateFloat(14)},
+      ];
+      const expectedHarvest = createFairHarvest(
+        timestampMs,
+        expectedReceipts,
+        strategy
+      );
+      const actual = harvest(strategy, credHistory, earnings, timestampMs);
+      expectHarvestsEqual(expectedHarvest, actual);
+    });
+    it("should divide according to cred if everyone is already fairly paid", () => {
+      const earnings = new Map([
+        [foo, fromApproximateFloat(5)],
+        [bar, fromApproximateFloat(2)],
+      ]);
+
+      const expectedReceipts = [
+        {address: foo, amount: fromApproximateFloat(10)},
+        {address: bar, amount: fromApproximateFloat(4)},
+      ];
+
+      const expectedHarvest = createFairHarvest(
+        timestampMs,
+        expectedReceipts,
+        strategy
+      );
+
+      const actual = harvest(strategy, credHistory, earnings, timestampMs);
+      expectHarvestsEqual(expectedHarvest, actual);
+    });
+    it("'top off' users who were slightly underpaid'", () => {
+      // Foo is exactly 1 grain behind where they "should" be
+      const earnings = new Map([
+        [foo, fromApproximateFloat(4)],
+        [bar, fromApproximateFloat(2)],
+      ]);
+
+      const strategy15 = {...strategy, amount: fromApproximateFloat(15)};
+
+      const expectedReceipts = [
+        {address: foo, amount: fromApproximateFloat(11)},
+        {address: bar, amount: fromApproximateFloat(4)},
+      ];
+
+      const expectedHarvest = createFairHarvest(
+        timestampMs,
+        expectedReceipts,
+        strategy15
+      );
+
+      const actual = harvest(strategy15, credHistory, earnings, timestampMs);
+      expectHarvestsEqual(expectedHarvest, actual);
+    });
+    it("should ignore cred scores from the future", () => {
+      const middleTimes = 29;
+
+      const strategy12 = {...strategy, amount: fromApproximateFloat(12)};
+
+      // In the last time slice, foo gets 0 cred and bar gets 2
+      const expectedReceipts = [
+        {address: foo, amount: fromApproximateFloat(10)},
+        {address: bar, amount: fromApproximateFloat(2)},
+      ];
+
+      const expectedHarvest = createFairHarvest(
+        middleTimes,
+        expectedReceipts,
+        strategy12
+      );
+
+      const actual = harvest(strategy12, credHistory, new Map(), middleTimes);
+      expectHarvestsEqual(expectedHarvest, actual);
+    });
+    it("should handle the case where one user has no historical earnings", () => {
+      const earnings = new Map([[foo, fromApproximateFloat(5)]]);
+
+      const expectedReceipts = [
+        {address: bar, amount: fromApproximateFloat(2)},
+      ];
+      const strategy2 = {...strategy, amount: fromApproximateFloat(2)};
+
+      const expectedHarvest = createFairHarvest(
+        timestampMs,
+        expectedReceipts,
+        strategy2
+      );
+
+      const actual = harvest(strategy2, credHistory, earnings, timestampMs);
+      expectHarvestsEqual(expectedHarvest, actual);
+    });
+    it("should not break if a user has earnings but no cred", () => {
+      const earnings = new Map([
+        [NodeAddress.fromParts(["zoink"]), fromApproximateFloat(10)],
+      ]);
+
+      const expectedReceipts = [
+        {address: foo, amount: fromApproximateFloat(10)},
+        {address: bar, amount: fromApproximateFloat(4)},
+      ];
+
+      const expectedHarvest = createFairHarvest(
+        timestampMs,
+        expectedReceipts,
+        strategy
+      );
+
+      const actual = harvest(strategy, credHistory, earnings, timestampMs);
+      expectHarvestsEqual(expectedHarvest, actual);
+    });
+  });
+});
